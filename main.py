ec76019b18658c721169bdca578ca3cb56c08e79
JulianBour <jb@ambimax.de>
2018-03-11 12:33:15 +0100

    rename user to dbActions - add timestamp

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import tagio
 import time
-import user
+import timestamp
+import dbActions
 
-connection = user.getConnection()
+connection = dbActions.getConnection()
 
 #endless loop for continuous reading
 while 1:
@@ -15,13 +16,15 @@ while 1:
 	#generate db table name for the current user
 	userTable = name + "_" + prename + "_" + id
 	#create new table if db table does not exist
-	if not user.checkIfUserTableAlreadyExists(connection, userTable):
-		user.createUserTable(connection, userTable)
+	if not dbActions.checkIfUserTableAlreadyExists(connection, userTable):
+		dbActions.createUserTable(connection, userTable)
 	#log time if table already exist
 	else:
 		###TODO remove 'print' for final version
 		print 'Timetable already exists and time will be logged now.'
-		user.addNewRecord(connection, userTable)
+		dbActions.addNewRecord(connection, userTable)
+
+
 	#system pause 
 	###TODO set on 5 sec for final version 
 	time.sleep(2)
