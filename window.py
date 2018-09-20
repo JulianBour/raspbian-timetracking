4a52b298f9144a06083c794556a84d16fb6f046f
root <root@raspberryavogadro>
2018-09-20 21:55:21 +0200

    login, logout; displaying

--- window.py
+++ window.py
@@ -6,6 +6,7 @@ from Tkinter import *
 from FullScreen import *
 from Functions import *
 from main import *
+import time as time
 
 #Create database table before start
 logging.createTableIfNotExists()
@@ -16,41 +17,66 @@ root=Tk()
 #CONST
 MAXWIDTH = root.winfo_screenwidth()
 MAXHEIGHT = root.winfo_screenheight()
+ORANGE = '#f4811e'
+BLACK = '#000000'
+WHITE = '#FFFFFF'
+GREY = '#949191'
 
 ###Frames erstellen
 #Head frame
 head = Frame(root, width = MAXWIDTH, height = MAXHEIGHT * 0.2 )
-head["bg"] = 'red'
+head["bg"] = ORANGE
 #Body frame
 body = Frame(root, width = MAXWIDTH, height = MAXHEIGHT * 0.6 )
-body["bg"] = 'green'
+body["bg"] = GREY
 #Footer frame
 footer = Frame(root, width = MAXWIDTH, height = MAXHEIGHT * 0.2 )
-footer["bg"] = 'red'
+footer["bg"] = ORANGE
 #Frames platzieren
 head.pack( side=TOP )
 footer.pack( side=BOTTOM )
 body.pack( side=BOTTOM )
+#Buttons
+loginButton = Button(footer, text='Login', width = 5, height = 2, command = lambda: staffLogin(),\
+                font = ("Helvetica", int(MAXHEIGHT * 0.05)))
+logoutButton = Button(footer, text='Logout', width = 5, height = 2, command = lambda: logoutAndQuit(), \
+                font = ("Helvetica", int(MAXHEIGHT * 0.05)))
+cancelButton = Button(footer, text='Abbrechen', width = 5, height = 2, command = lambda: cancel(), \
+                font = ("Helvetica", int(MAXHEIGHT * 0.05)))
+#TODO def add buttons
+###
 
-def loginAndQuit():
-    logging.login(5)
-    root.destroy()
+def cancel():
+    clearFooter()
+    createFooter()
 
+def createFooter():
+    loginButton.place(relx=0, anchor=NW)
+    logoutButton.place(relx=1, anchor=NE)
+
+def clearFooter():
+    loginButton.destroy()
+    cancelButton.destroy()
+    logoutButton.destroy()
+
+def staffLogin():
+    kp = Label(body , text="Bitte jetzt den Finger auf den Scanner legen")
+    kp.place(relx = 0.5,rely = 0.5, anchor = CENTER)
+    cancelButton.place(relx = 0.5,rely = 0.5, anchor = CENTER)
+
+    
+    
+    #TODO staffId = scanner.scanFinger()
+    #TODO logging.login(staffId)
+    #TODO If success then delete label and print success and delete after 5 seconds
+    
 def logoutAndQuit():
     logging.logout(5)
     root.destroy()
     
-###Buttons
-#Login Button
-login = Button(footer, text='Login', width = 5, height = 2, command = lambda: loginAndQuit(), \
-               font = ("Helvetica", int(MAXHEIGHT * 0.05)))
-login.place(relx=0, anchor=NW)
 
-#Logout Button
-logout = Button(footer, text='Logout', width = 5, height = 2, command = lambda: logoutAndQuit(), \
-               font = ("Helvetica", int(MAXHEIGHT * 0.05)))
-logout.place(relx=1, anchor=NE)
-
+#create Footer
+createFooter()
 
 app=FullScreenApp(root)
 root.mainloop()
