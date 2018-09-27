2acbd0c2977e9e6bcfc1b3970c1b6d33837159eb
Julian Bour <jb@ambimax.de>
2018-09-27 13:51:12 +0200

    added cancel Button - defined functions for footer and body

--- window.py
+++ window.py
@@ -37,46 +37,75 @@ head.pack( side=TOP )
 footer.pack( side=BOTTOM )
 body.pack( side=BOTTOM )
 #Buttons
-loginButton = Button(footer, text='Login', width = 5, height = 2, command = lambda: staffLogin(),\
+loginButton = Button(footer, text='Login', width = 7, height = 2, command = lambda: staffLogin(),\
                 font = ("Helvetica", int(MAXHEIGHT * 0.05)))
-logoutButton = Button(footer, text='Logout', width = 5, height = 2, command = lambda: logoutAndQuit(), \
+logoutButton = Button(footer, text='Logout', width = 7, height = 2, command = lambda: staffLogout(), \
                 font = ("Helvetica", int(MAXHEIGHT * 0.05)))
-cancelButton = Button(footer, text='Abbrechen', width = 5, height = 2, command = lambda: cancel(), \
+cancelButton = Button(footer, text='Abbruch', width = 7, height = 2, command = lambda: cancel(), \
                 font = ("Helvetica", int(MAXHEIGHT * 0.05)))
+devExitButton = Button(head, text='EXIT', width = 7, height = 2, command = lambda: devExit(), \
+                font = ("Helvetica", int(MAXHEIGHT * 0.05)))
+#Labels
+bodyMessage="Willkommen"
+authMessageLabel = Label(body, text=bodyMessage, font = ("Helvetica", int(MAXHEIGHT * 0.1)))
 #TODO def add buttons
 ###
 
 def cancel():
-    clearFooter()
-    createFooter()
+    clearLogFooter()
+    createStandardFooter()
+    createStandardBody()
 
-def createFooter():
+def devExit():
+    root.destroy()
+
+def createStandardBody():
+    bodyMessage="Willkommen"
+    authMessageLabel.config(text=bodyMessage, font = ("Helvetica", int(MAXHEIGHT * 0.1)))
+    authMessageLabel.place(relx = 0.5,rely = 0.5, anchor = CENTER)
+
+def createLogFooter():
+    cancelButton.place(relx = 0.4,rely = 0, anchor = NW)
+
+def clearLogFooter():
+    cancelButton.place_forget()
+
+def createStandardFooter():
     loginButton.place(relx=0, anchor=NW)
     logoutButton.place(relx=1, anchor=NE)
 
-def clearFooter():
-    loginButton.destroy()
-    cancelButton.destroy()
-    logoutButton.destroy()
+def clearStandardFooter():
+    loginButton.place_forget()
+    logoutButton.place_forget()
 
 def staffLogin():
-    kp = Label(body , text="Bitte jetzt den Finger auf den Scanner legen")
-    kp.place(relx = 0.5,rely = 0.5, anchor = CENTER)
-    cancelButton.place(relx = 0.5,rely = 0.5, anchor = CENTER)
+    bodyMessage="Bitte jetzt den Finger auf den Scanner legen"
+    authMessageLabel.config(text=bodyMessage, font = ("Helvetica", int(MAXHEIGHT * 0.05)))
 
-    
-    
+    clearStandardFooter()
+    createLogFooter()
+
+def staffLogout():
+    bodyMessage="Bitte jetzt den Finger auf den Scanner legen"
+    authMessageLabel.config(text=bodyMessage, font = ("Helvetica", int(MAXHEIGHT * 0.05)))
+
+    clearStandardFooter()
+    createLogFooter()
+
     #TODO staffId = scanner.scanFinger()
     #TODO logging.login(staffId)
     #TODO If success then delete label and print success and delete after 5 seconds
-    
+
+#TODO remove function in final version
 def logoutAndQuit():
     logging.logout(5)
-    root.destroy()
-    
 
 #create Footer
-createFooter()
+createStandardFooter()
+createStandardBody()
+
+#TOTO remove dev buttons
+devExitButton.place(relx=0.5, anchor=NW)
 
 app=FullScreenApp(root)
 root.mainloop()
